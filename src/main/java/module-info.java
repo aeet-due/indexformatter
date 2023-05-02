506a41de2f5a71db5322bc3ced31d3fcd1f69623
Bernhard Fisseni <bernhard.fisseni@mail.de>
2023-05-02 14:12:32 +0200

    nicer modularization

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * @author bfi
+ *
+ */
+module indexformatter {
+	exports org.korpora.aeet.ediarum;
+	requires org.korpora.useful;
+	requires java.xml;
+	requires Saxon.HE;
+	requires info.picocli;
+}
